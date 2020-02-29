0253f752caf77bdd5d47f293f11cdd077b9b342d
Isaiah Norton <isaiah@tiledb.com>
2020-02-29 17:34:36 +0000

    WIP

--- recipes/tiledb-vcf/build-tiledbvcf-py.sh
+++ recipes/tiledb-vcf/build-tiledbvcf-py.sh
@@ -6,4 +6,6 @@ set -x
 export TILEDBVCF_CMAKE_PREFIX_PATH="${PREFIX}"
 export TILEDBVCF_FORCE_EXTERNAL_HTSLIB="OFF"
 
-$PYTHON setup.py install --tiledb="$PREFIX" --single-version-externally-managed --record record.txt
+cd apis/python
+
+$PYTHON setup.py install --single-version-externally-managed --record record.txt
