570464103aee8deaf9e86af82988dc9529fe368b
Isaiah Norton <isaiah@tiledb.com>
2020-02-29 18:54:51 +0000

    WIP

---
+++ recipes/tiledb-vcf/install-libtiledbvcf.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -exo pipefail
+
+echo "\n\n\n---------------------aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n\n\n\n"
+
+cd libtiledbvcf-build
+make install-libtiledbvcf
+echo "\n\n\n---------------------aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n\n\n\n"
