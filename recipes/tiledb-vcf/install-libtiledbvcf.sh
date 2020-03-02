25ba8a532836da2b0df9a66509bde5789df5c1d4
Isaiah Norton <isaiah@tiledb.com>
2020-03-02 21:08:35 +0000

    WIP remove debug output

--- recipes/tiledb-vcf/install-libtiledbvcf.sh
+++ recipes/tiledb-vcf/install-libtiledbvcf.sh
@@ -2,8 +2,5 @@
 
 set -exo pipefail
 
-echo "\n\n\n---------------------aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n\n\n\n"
-
 cd libtiledbvcf-build
 make install-libtiledbvcf
-echo "\n\n\n---------------------aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n\n\n\n"
